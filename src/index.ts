b2134ecd7b3ffe7e5bbfeac4e8532cbb819ef091
AyodejiOmole <omoledaniel13@gmail.com>
2024-02-17 15:52:59 +0100

    created endpoint to retrieve all authors while creating a service and route file in the author folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
+import { authorRouter } from "./author/author.router";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/authors", authorRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
