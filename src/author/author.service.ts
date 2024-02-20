adb52c2bf523475593ad94b32773b9e73c6b2fe9
AyodejiOmole <omoledaniel13@gmail.com>
2024-02-20 15:05:35 +0100

    added service to update the details of a specific author on the database

--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -40,3 +40,23 @@ export const createAuthor = async (author: Omit<Author, "id">): Promise<Author> 
     });
 };
 
+export const updateAuthor = async (author: Omit<Author, "id">, id: string): Promise<Author> => {
+    const { firstName, lastName } = author;
+
+    return db.author.update({
+        where: {
+            id,
+        },
+        data: {
+            firstName,
+            lastName,
+        },
+        select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            createdAt: true,
+            updatedAt: true,
+        },
+    });
+};
