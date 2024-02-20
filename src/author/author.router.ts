3896d1c7fa5855d1e16249694bce916f0b821ead
AyodejiOmole <omoledaniel13@gmail.com>
2024-02-20 14:53:53 +0100

    created a new route to get a specific author using an id

--- src/author/author.router.ts
+++ src/author/author.router.ts
@@ -6,10 +6,27 @@ import * as AuthorService from "./author.service";
 
 export const authorRouter = express.Router();
 
+
+// GET all authors on the database
 authorRouter.get("/", async (request: Request, response: Response) => {
     try {
         const authors = await AuthorService.listAuthors();
         return response.status(200).json(authors);
+    } catch (error: any) {
+        return response.status(500).json(error.message);
+    }
+});
+
+// GET a specific author on the database
+authorRouter.get("/:id", async (request: Request, response: Response) => {
+    const id: string = request.params.id;
+    try {
+        const author = await AuthorService.getAuthor(id);
+        if(author) {
+            return response.status(200).json(author);
+        }
+
+        return response.status(404).json("Author could not be found!");
     } catch (error: any) {
         return response.status(500).json(error.message);
     }
