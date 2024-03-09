b49cb31f86f29ab46bfa59a51d196d0645a9bd7e
AyodejiOmole <omoledaniel13@gmail.com>
2024-03-09 09:38:33 +0100

    created services to create, update, and delete a book from the database

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -5,6 +5,7 @@ type Book = {
     title: string;
     isFiction: boolean;
     datePublished: Date;
+    authorId: string
 }
 
 export const listBooks = async (): Promise<Book[]> => {
@@ -14,6 +15,7 @@ export const listBooks = async (): Promise<Book[]> => {
             title: true,
             isFiction: true,
             datePublished: true,
+            authorId: true,
         },
     });
 };
@@ -25,3 +27,55 @@ export const getBook = async (id: string): Promise<Book | null> => {
         },
     });
 };
+
+export const createBook = async (book: Omit<Book, "id">): Promise<Book> => {
+    const { title, isFiction, datePublished, authorId } = book;
+
+    return db.book.create({
+        data: {
+            title,
+            isFiction,
+            datePublished,
+            authorId,
+        },
+        select: {
+            id: true,
+            title: true,
+            isFiction: true,
+            datePublished: true,
+            authorId: true,
+        }
+    });
+};
+
+export const updateBook = async (book: Omit<Book, "id">, id: string): Promise<Book> => {
+    const { title, isFiction, datePublished } = book;
+
+    return db.book.update({
+        where: {
+            id,
+        },
+        data: {
+            title,
+            isFiction,
+            datePublished,
+        },
+        select: {
+            id: true,
+            title: true,
+            isFiction: true,
+            datePublished: true,
+            createdAt: true,
+            updatedAt: true,
+            authorId: true,
+        }
+    });
+};
+
+export const deleteAuthor = async (id: string) => {
+    return db.book.delete({
+        where: {
+            id,
+        },
+    });
+},
